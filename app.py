f0acd547c71fbdfae5dbe2a662493989e85e5076
rajat-saxena14 <49250295+rajat-saxena14@users.noreply.github.com>
2021-05-18 07:54:03 +0530

    New responsive design

--- app.py
+++ app.py
@@ -1,85 +1,128 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, render_template, abort, session, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from datetime import datetime
+import sqlalchemy
+from flask_mail import Mail, Message
+from config import mail_username, mail_password
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+app.config['SECRET_KEY'] = "RAJATSAXENA14"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = "smtp-mail.outlook.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+
+mail = Mail(app)
 db = SQLAlchemy(app)
-
-admin=Admin(app)
-
+admin = Admin(app)
 
 
-class BlogPost(db.Model):
+class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    subtitle = db.Column(db.String(255), nullable=True)
     content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
-
-    def __repr__(self):
-        return 'Blog post ' + str(self.id)
-
-admin.add_view(ModelView(BlogPost, db.session))
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime)
+    slug = db.Column(db.String(255))
+    views = db.Column(db.Integer,default=0)
+    comments = db.Column(db.Integer,default=0)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    email = db.Column(db.String(200), unique=False, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id', ondelete='CASCADE'), nullable=False)
+    posts = db.relationship('Posts', backref=db.backref('posts',lazy=True, passive_deletes=True))
+    date_pub = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class SecureModelView(ModelView):
+    def is_accessible(self):
+        if "logged_in" in session:
+            return True
+        else:
+            abort(403)
 
 
-@app.route('/posts', methods=['GET', 'POST'])
-def posts():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_author = request.form['author']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
+admin.add_view(SecureModelView(Posts, db.session))
+admin.add_view(SecureModelView(Comments, db.session))
+
+
+@app.route("/")
+def homepage():
+    posts = Posts.query.all()
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/post/<string:slug>", methods=['POST','GET'])
+def post(slug):
+    try:
+        post = Posts.query.filter_by(slug=slug).one()
+        comment = Comments.query.filter_by(post_id=post.id).all()
+        post.views = post.views + 1
         db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        return render_template('posts.html', posts=all_posts)
+        Thanks = ""
+        if request.method == "POST":
+            post_id = post.id
+            name = request.form.get('name')
+            email = request.form.get('email')
+            message = request.form.get('message')
+            comment = Comments(name=name, email=email, message=message, post_id=post_id)
+            db.session.add(comment)
+            post.comments = post.comments + 1
+            db.session.commit()
+            flash('Posted sucessfully!', 'success')
+            return redirect(request.url)
+        return render_template("post.html", post=post, comment=comment, Thanks=Thanks)
+    except sqlalchemy.orm.exc.NoResultFound:
+        abort(404)
 
 
-@app.route('/posts/delete/<int:id>')
-def delete(id):
-    post = BlogPost.query.get_or_404(id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect('/posts')
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        msg = Message(
+            subject=f"Mail from {name}", body=f"Name: {name}\nE-Mail: {email}\nPhone: {phone}\n\n\n{message}", sender=mail_username, recipients=['rajat14.rs@outlook.com'])
+        mail.send(msg)
+        return render_template("contact.html", success=True)
+
+    return render_template("contact.html")
 
 
-@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    post = BlogPost.query.get_or_404(id)
-
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('edit.html', post=post)
+@app.route("/auth", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form.get("username") == "admin" and request.form.get("password") == "rajatsaxena14":
+            session['logged_in'] = True
+            return redirect("/admin")
+        else:
+            return render_template("login.html", failed=True)
+    return render_template("login.html")
 
 
-@app.route('/posts/new', methods=['GET', 'POST'])
-def new_post():
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('new_post.html')
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
